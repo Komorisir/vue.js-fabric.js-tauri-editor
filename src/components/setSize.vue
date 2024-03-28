8b9afd570725768c2c9e27c636c7e43c767151bb
sunfukang <s_sunfukang@wps.cn>
2024-03-28 16:39:26 +0800

    feat: 【调整画布尺寸】弹窗界面修改，input输入框样式调整

--- src/components/setSize.vue
+++ src/components/setSize.vue
@@ -1,12 +1,20 @@
 <template>
   <div v-if="!mixinState.mSelectMode">
     <Divider plain orientation="left">{{ $t('size') }}</Divider>
-    <Form :label-width="40" class="form-wrap">
-      <FormItem :label="$t('width')" prop="name">
-        <InputNumber disabled v-model="width" @on-change="setSize"></InputNumber>
+    <Form class="form-wrap">
+      <FormItem prop="name" style="margin-right: 10px">
+        <Input type="number" disabled v-model="width" @on-change="setSize">
+          <template #prepend>
+            <span>{{ $t('width') }}</span>
+          </template>
+        </Input>
       </FormItem>
-      <FormItem :label="$t('height')" prop="name">
-        <InputNumber disabled v-model="height" @on-change="setSize"></InputNumber>
+      <FormItem prop="name">
+        <Input type="number" disabled v-model="height" @on-change="setSize">
+          <template #prepend>
+            <span>{{ $t('height') }}</span>
+          </template>
+        </Input>
       </FormItem>
     </Form>
     <Button type="primary" @click="() => (showModal = true)">调整尺寸</Button>
@@ -22,20 +30,27 @@
         <Button
           v-for="(item, i) in presetSize"
           :key="`${i}presetSize`"
-          size="small"
+          size="default"
           style="text-align: left"
           @click="setSizeBy(item.width, item.height)"
         >
           {{ item.label }}:{{ item.width }}x{{ item.height }}
         </Button>
       </ButtonGroup>
-
-      <Form :label-width="40" class="form-wrap" style="justify-content: flex-start">
-        <FormItem :label="$t('width')" prop="name" style="margin-right: 10px">
-          <InputNumber :min="1" :max="5000" v-model="modalData.width"></InputNumber>
+      <Form class="form-wrap" style="justify-content: flex-start">
+        <FormItem prop="name" style="margin-right: 10px">
+          <Input type="number" v-model="modalData.width">
+            <template #prepend>
+              <span>{{ $t('width') }}</span>
+            </template>
+          </Input>
         </FormItem>
-        <FormItem :label="$t('height')" prop="name">
-          <InputNumber :min="1" :max="5000" v-model="modalData.height"></InputNumber>
+        <FormItem prop="name">
+          <Input type="number" v-model="modalData.height">
+            <template #prepend>
+              <span>{{ $t('width') }}</span>
+            </template>
+          </Input>
         </FormItem>
       </Form>
     </Modal>
@@ -64,12 +79,12 @@ let width = ref(DefaultSize.width);
 let height = ref(DefaultSize.height);
 let presetSize = reactive([
   {
-    label: t('red_book_vertical'),
+    label: t('type_vertical'),
     width: 900,
     height: 1200,
   },
   {
-    label: t('red_book_horizontal'),
+    label: t('type_horizontal'),
     width: 1200,
     height: 900,
   },
@@ -137,6 +152,7 @@ const handleConfirm = () => {
 }
 
 .form-wrap {
+  width: 250px;
   display: flex;
   justify-content: space-around;
   align-content: center;
